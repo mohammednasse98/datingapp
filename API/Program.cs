a65d6abff6349fb147a472574cebf3f9d9f4fa3b
Mohammed Nasser <mohammednasse98@gmail.com>
2025-10-25 04:29:19 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,11 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddCors();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200/"));
 
 app.MapControllers();
 
